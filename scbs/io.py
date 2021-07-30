c46072855caf12a9cd3dda1c99b5ccef1fa33e4f
Alexey <alexey.mipt@gmail.com>
2021-07-30 16:41:22 +0200

    add h5py to prepare

--- scbs/io.py
+++ scbs/io.py
@@ -6,15 +6,15 @@ def write_sparse_hdf5(h5object: h5py.AttributeManager, matrix):
     h5object["indptr"] = matrix.indptr
     h5object["data"] = matrix.data
     h5object["indices"] = matrix.indices
-    h5object["format"] = matrix.format
+    h5object.attrs["format"] = matrix.format
+    h5object["shape"] = matrix.shape
 
 
 def read_sparse_hdf5(h5object: h5py.AttributeManager):
     """Read the matrix from the provided h5py File or Group."""
     try:
-        matformat = h5object["format"]
         constructor = {"csc": sparse.csc_matrix, "csr": sparse.csr_matrix}[
-            matformat
+            h5object.attrs["format"]
         ]
     except KeyError:
         raise KeyError(
@@ -22,7 +22,8 @@ def read_sparse_hdf5(h5object: h5py.AttributeManager):
         )
     try:
         return constructor(
-            h5object["data"], h5object["indices"], h5object["indptr"]
+            (h5object["data"], h5object["indices"], h5object["indptr"]),
+            shape=h5object["shape"],
         )
     except KeyError:
         raise Exception(
