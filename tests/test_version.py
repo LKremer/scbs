79711523d4a46a8232c2e6355172d8b131fbc786
Lukas <l.kremer@dkfz.de>
2022-03-23 18:21:46 +0100

    proper version test

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,5 +1,11 @@
 from scbs import __version__
 
 
+with open("pyproject.toml", "r") as toml:
+    for line in toml:
+        if line.startswith("version = "):
+            toml_version = line.strip().split()[-1].strip("\"")
+
+
 def test_version():
-    assert __version__ == '0.3.0'
+    assert __version__ == toml_version
